e79293da163f1d73c27d742a0b6eb1aac04a0ca7
YamanAyoun <yamanayoun0@gmail.com>
2023-04-10 20:21:08 +0000

    fix problem

--- js/app.js
+++ js/app.js
@@ -1,43 +1,19 @@
 'use strict';
+
+let employeeArr = [];
 function Employee(id, fullName, department, level, imageURL,salary) {
     this.id = id;
     this.fullName = fullName;
     this.department = department;
     this.level = level;
     this.imageURL = imageURL;
-    this.salary = salary;
+    this.salary = 0;
+    employeeArr.push(this)
   }
 
 
-  Employee.prototype.calculateSalary = function() {
-    let minSalary, maxSalary;
-  
-    switch (this.level) {
-      case "Junior":
-        minSalary = 500;
-        maxSalary = 1000;
-        break;
-      case "Mid-Senior":
-        minSalary = 1000;
-        maxSalary = 1500;
-        break;
-      case "Senior":
-        minSalary = 1500;
-        maxSalary = 2000;
-        break;
-      default:
-        minSalary = 0;
-        maxSalary = 0;
-        break;
-    }
-  
-    const salary = Math.floor(Math.random() * (maxSalary - minSalary)) + minSalary;
-    const taxAmount = salary * 0.075;
-   
-  
-    return {salary};
-  };
 
+  
   const employee1 = new Employee(1000, "Ghazi Samer", "Administration", "Senior", "./img/company.png");
   const employee2 = new Employee(1001, "Lana Ali", "Finance", "Senior", "./img/company.png");
   const employee3 = new Employee(1002, "Tamara Ayoub", "Marketing", "Senior", "./img/company.png");
@@ -46,9 +22,26 @@ function Employee(id, fullName, department, level, imageURL,salary) {
   const employee6 = new Employee(1003, "Rana Saleh", "Development", "Junior", "./img/company.png");
   const employee7 = new Employee(1004, "Hadi Ahmad", "Finance", "Mid-Senior", "./img/company.png");
 
-    console.log(employee1);
-    console.log(employee1.calculateSalary());
     
-  
-  
-document.write('<h1> information for each employee</h1>');
+  Employee.prototype.calculateSalary = function(){
+    if(this.level == 'Senior'){
+      this.salary = randomNumber(1500,2000)-(randomNumber(1500,2000)*0.075);
+    }else if(this.level == 'Mid-Senior'){
+      this.salary = randomNumber(1000,1500)-(randomNumber(1000,1500));
+    }else if(this.level == 'Junior'){
+      this.salary = randomNumber(500,1000)-(randomNumber(500,1000));
+    }
+    return this.salary;
+  }
+
+  function randomNumber(min, max){
+    return Math.floor(Math.random() * (max-min)+min);
+  }
+  Employee.prototype.render = function(){
+    document.write(`name is ${this.fullName}`)
+  }
+
+  for(let i = 0; i < employeeArr.length; i++){
+    document.write(`<hr>`)
+    employeeArr[i].render();
+  }
